fd18897aa6a2d6c482858b71fe77586555455cb7
Ariella <ariellaw@gmail.com>
2018-10-19 14:39:18 +0300

    updated the contact form and made the contact element in the nav bar open the contact form - also added 2 specialties

--- js/offCanvas.js
+++ js/offCanvas.js
@@ -1,3 +1,24 @@
+$(document).ready(function(){ 
+    $('#characterLeft').text('200 characters left');
+    $('#message').keydown(function () {
+        var max = 200;
+        var len = $(this).val().length;
+        if (len >= max) {
+            $('#characterLeft').text('You have reached the limit');
+            $('#characterLeft').addClass('red');
+            $('#btnSubmit').addClass('disabled');            
+        } 
+        else {
+            var ch = max - len;
+            $('#characterLeft').text(ch + ' characters left');
+            $('#btnSubmit').removeClass('disabled');
+            $('#characterLeft').removeClass('red');            
+        }
+    });    
+});
+
+
+
 function openCanvas(){
     document.querySelector('.offcanvas-btn').classList.toggle('offcanvas-btn-open');
     document.querySelector('.offcanvas-aside').classList.toggle('offcanvas-aside-open');    
